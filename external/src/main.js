7fb263715aa978696a244960cca07340f334ccb6
Sergio Sarmiento <gersiomarsiento@gmail.com>
2024-06-28 15:37:17 -0300

    feat: collection swticher + contact + faqs

--- external/src/main.js
+++ external/src/main.js
@@ -12,6 +12,54 @@ import intersect from '@alpinejs/intersect';
 import './js/product';
 import './js/cart';
 
+// Featured Collection Switcher Slider Swiper
+Alpine.data('feat_collection_slider', () => ({
+  init() {
+    new Swiper('.swiper_feat_collection_slider_1', {
+      direction: 'horizontal',
+      loop: true,
+      slidesPerView: 2,
+      spaceBetween: 0,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+        1280: {
+          slidesPerView: 5,
+        },
+      },
+    });
+    new Swiper('.swiper_feat_collection_slider_2', {
+      direction: 'horizontal',
+      loop: true,
+      slidesPerView: 2,
+      spaceBetween: 0,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+        1280: {
+          slidesPerView: 5,
+        },
+      },
+    });
+  },
+}));
+
 Alpine.plugin(focus);
 Alpine.plugin(collapse);
 Alpine.plugin(anchor);
